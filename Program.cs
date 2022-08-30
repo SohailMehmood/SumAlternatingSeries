aa51c69a2ed8c51da3d616bcbc6665853679ffb2
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-08-30 13:48:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float sum = 0;
+
+       for(float i = 1; i <= 1e6; i++)
+        {
+            if (i % 2 == 1)
+            {
+                sum += 1 / (2 * i - 1);
+            }
+            else
+            {
+                sum -= 1 / (2 * i - 1);
+            }
+            
+            float answer = sum * 4;
+            sum++;
+            
+            Console.WriteLine(answer); 
+             
+        }
+
+
+    }
+}
